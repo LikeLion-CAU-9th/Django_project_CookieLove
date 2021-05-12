a35cb07d839d77ef3f3feec8f961c4368acada50
oereo <dlstpgns0406@gmail.com>
2021-05-12 17:28:47 +0900

    refactor: solve conflict problem and Rebase

--- CL_first_project/CL_first_project/urls.py
+++ CL_first_project/CL_first_project/urls.py
@@ -6,14 +6,9 @@ from main.views import mainHome
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-<<<<<<< HEAD
     path('chaeyeon/', include("chaeyeon.urls")),
     path('taeyoung/', include("taeyoung.urls")),
     path('youngkwon/', include("youngkwon.urls")),
-=======
-    # path('', include("chaeyeon.urls")),
     path('taeyoung/', views.intro, name="intro"),
     path('', mainHome, name="main-home")
-    # path('', include("youngkwon.urls")),
->>>>>>> feat: connect main page
 ]
--- CL_first_project/main/apps.py
+++ CL_first_project/main/apps.py
@@ -1,12 +1,6 @@
 from django.apps import AppConfig
 
 
-<<<<<<< HEAD:CL_first_project/chaeyeon/apps.py
-class ChaeyeonConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'chaeyeon'
-=======
 class MainConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'main'
->>>>>>> feat: connect main page:CL_first_project/main/apps.py
