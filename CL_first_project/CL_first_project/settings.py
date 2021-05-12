4ac4706672f8ae1ab5a1bf7c13f8759cad9dbee7
oereo <dlstpgns0406@gmail.com>
2021-05-12 17:11:00 +0900

    feat: connect main page

--- CL_first_project/CL_first_project/settings.py
+++ CL_first_project/CL_first_project/settings.py
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
     'youngkwon',
     'taeyoung',
     'chaeyeon',
+    'main'
 ]
 
 MIDDLEWARE = [
--- CL_first_project/CL_first_project/urls.py
+++ CL_first_project/CL_first_project/urls.py
@@ -1,10 +1,19 @@
 from django.contrib import admin
 from django.urls import path, include
 from taeyoung import views
+from main.views import mainHome
+    
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+<<<<<<< HEAD
     path('chaeyeon/', include("chaeyeon.urls")),
     path('taeyoung/', include("taeyoung.urls")),
     path('youngkwon/', include("youngkwon.urls")),
+=======
+    # path('', include("chaeyeon.urls")),
+    path('taeyoung/', views.intro, name="intro"),
+    path('', mainHome, name="main-home")
+    # path('', include("youngkwon.urls")),
+>>>>>>> feat: connect main page
 ]
